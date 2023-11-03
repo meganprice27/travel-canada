8ade720a4a8eecf3e455cd73263a7928bca4b864
meganprice27 <149601780+meganprice27@users.noreply.github.com>
2023-11-03 17:53:07 +0000

    Styled main body and added new image

--- src/java.js
+++ src/java.js
@@ -1,14 +1,14 @@
 function findOutMore() {
-        let license = prompt("Do you have a drivers license?");
-        license = license.toLowerCase();
-        if (license === "yes") {
-          alert("You are able to drive in Canada! 🎉");
-          let email = prompt("Please provide your email");
-          alert("Thank you, we will be in touch with more road trip info! 🚗");
-        } else {
-          alert("Sorry, you aren't able to drive in Canada 😔");
-        }
-      }
-      let moreButton = document.querySelector("button");
+  let license = prompt("Do you have a drivers license?");
+  license = license.toLowerCase();
+  if (license === "yes") {
+    alert("You are able to drive in Canada! 🎉");
+    let email = prompt("Please provide your email");
+    alert("Thank you, we will be in touch with more road trip info! 🚗");
+  } else {
+    alert("Sorry, you aren't able to drive in Canada 😔");
+  }
+}
+let moreButton = document.querySelector("button");
 
-      moreButton.addEventListener("click", findOutMore);
+moreButton.addEventListener("click", findOutMore);
