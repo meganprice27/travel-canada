a6b4d0f15807456de24f15e21869728325397715
meganprice27 <149601780+meganprice27@users.noreply.github.com>
2023-11-10 11:51:25 +0000

    Added name to alert in Java Script

--- src/java.js
+++ src/java.js
@@ -3,8 +3,9 @@ function findOutMore() {
   license = license.toLowerCase();
   if (license === "yes") {
     alert("You are able to drive in Canada! 🎉");
+    let name = prompt("Please enter your name");
     let email = prompt("Please provide your email");
-    alert("Thank you, we will be in touch with more road trip info! 🚗");
+    alert(`Thank you ${name}, we will be in touch with more road trip info! 🚗`);
   } else {
     alert("Sorry, you aren't able to drive in Canada 😔");
   }
